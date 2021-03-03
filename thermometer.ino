896eabeed46c4b8a7abffc64aad95dddbb470b7f
Matěj Drábek <matej1drabek@seznam.cz>
2021-03-03 22:13:03 +0100

    Temperature is now displayed on 7seg displays

--- thermometer.ino
+++ thermometer.ino
@@ -1,6 +1,11 @@
+#include <math.h>
+
 const int THERMOMETER_PIN = A1;
 const int TEMPERATURE_BUFFER_SIZE = 50; //Voltage from the thermometer tends to jump sometimes
                                     //so this should average it and even it out
+
+const int firstDisplayPins[4] = {9,10,11,12};
+const int seccondDisplayPins[4] = {5,6,7,8};
 
 float temperatureArray[TEMPERATURE_BUFFER_SIZE];
 byte temperatureArrayIndex = 0;
@@ -18,7 +23,10 @@ void setup() {
   Serial.begin(9600);
   pinMode(THERMOMETER_PIN, INPUT);
   pinMode(2, OUTPUT);
-  
+
+
+  setUpDisplayPins(firstDisplayPins);
+  setUpDisplayPins(seccondDisplayPins);
 }
 
 ISR(TIMER1_OVF_vect)        // interrupt service routine that wraps a user defined function supplied by attachInterrupt
@@ -28,7 +36,18 @@ ISR(TIMER1_OVF_vect)        // interrupt service routine that wraps a user defin
   float temperatureCentigrade = getCentigradeTemperature(thermometerVolts);
   insertNewTemperatureToArray(temperatureCentigrade);
   Serial.print("C average: ");
-  Serial.println(getAverageTemperature());
+  
+  float test = getAverageTemperature();
+  Serial.println(test);
+  /*Serial.println( (int(test) % 100) / 10);
+  
+  Serial.println(int(test) % 10);
+  
+  Serial.println(int((test * 10)) % 10);
+  
+  Serial.println("================");*/
+  displayNumber(firstDisplayPins, (int(test) % 100) / 10);
+  displayNumber(seccondDisplayPins, int(test) % 10);
 }
 
 void loop() {
@@ -36,6 +55,23 @@ void loop() {
   digitalWrite(2, !digitalRead(2));
   delay(500);
 
+}
+
+void setUpDisplayPins(int pins[4])
+{
+  for(int i = 0; i < 4; i++)
+  {
+    pinMode(pins[i], OUTPUT);
+  }
+}
+
+void displayNumber(int pins[4], int number)
+{
+  for(int i = 0; i < 4; i++)
+  {
+     digitalWrite(pins[i], number %2);
+     number /=2;
+  }
 }
 
 float getVoltage()
